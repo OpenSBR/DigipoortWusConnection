35f5f2e5b2995a0fd7e60b7176e456d6cc46d315
Open SBR <info@opensbr.org>
2017-10-01 20:21:43 +0200

    Initial setup

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LogiusDigipoort")]
+[assembly: AssemblyDescription("Sample code for Digipoort communication - code based on Logius sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenSBR.org")]
+[assembly: AssemblyProduct("LogiusDigipoort")]
+[assembly: AssemblyCopyright("Copyright © OpenSBR 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("542ca47b-4dbd-49cc-809c-1675f583b966")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
